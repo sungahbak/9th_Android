26d17a9cb144be02c423de982de3aa65d79c5fa3
sungahbak <pasunga12@gmail.com>
2025-10-11 16:09:09 +0900

    keyword: 4주차 키워드 제출

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation("androidx.viewpager2:viewpager2:1.0.0")
+    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/umc/HomeFragment.kt
+++ app/src/main/java/com/example/umc/HomeFragment.kt
@@ -5,13 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.umc.databinding.FragmentHomeBinding
 import java.util.zip.Inflater
 import com.example.umc.AlbumFragment
+import com.example.umc.com.example.umc.Album
+import java.util.ArrayList
+import com.google.gson.Gson
 
 class HomeFragment : Fragment() {
     lateinit var binding : FragmentHomeBinding
+
+    private var albumDatas = ArrayList<Album>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,15 +26,35 @@ class HomeFragment : Fragment() {
     ) : View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        binding.homePannelAlbumImgIv2.setOnClickListener {
-            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,AlbumFragment()).commitAllowingStateLoss()
+//        binding.homePannelAlbumImgIv2.setOnClickListener {
+//            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm,AlbumFragment()).commitAllowingStateLoss()
+//       }
+
+        albumDatas.apply{
+            add(Album("Butter", "방탄소년단 (BTS)", R.drawable.img_album_exp))
+            add(Album("Lilac", "아이유 (IU)", R.drawable.img_album_exp2))
+            add(Album("Next Level", "에스파 (AESPA)", R.drawable.img_album_exp3))
+            add(Album("Boy with Luv", "방탄소년단 (BTS)", R.drawable.img_album_exp4))
+            add(Album("BBoom BBoom", "모모랜드 (MOMOLAND)", R.drawable.img_album_exp5))
+            add(Album("Weekend", "태연 (Tae Yeon)", R.drawable.img_album_exp6))
         }
 
+        val albumRVAdapter = AlbumRVAdapter(albumDatas)
+        binding.homeTodayMusicAlbumRv.adapter = albumRVAdapter
+        binding.homeTodayMusicAlbumRv.layoutManager = LinearLayoutManager(context,
+            LinearLayoutManager.HORIZONTAL,false)
+
+        albumRVAdapter.setMyItemClickListener(object : AlbumRVAdapter.MyItemClickListener{
+            override fun onItemClick(album: Album) {
+                changeAlbumFragment(album)
+            }
+
+            override fun onRemoveAlbum(position: Int) {
+                albumRVAdapter.removeItem(position)
+            }
+
+        })
         val bannerAdapter = BannerVPAdapter(this)
-        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
-        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
-        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
-        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
         bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
         bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
         binding.homeBannerVp.adapter = bannerAdapter
@@ -36,4 +62,16 @@ class HomeFragment : Fragment() {
 
         return binding.root
     }
+}
+
+private fun HomeFragment.changeAlbumFragment(album: Album) {
+    (context as MainActivity).supportFragmentManager.beginTransaction()
+        .replace(R.id.main_frm, AlbumFragment().apply {
+            arguments = Bundle().apply {
+                val gson = Gson()
+                val albumJson = gson.toJson(album)
+                putString("album", albumJson)
+            }
+        })
+        .commitAllowingStateLoss()
 }
